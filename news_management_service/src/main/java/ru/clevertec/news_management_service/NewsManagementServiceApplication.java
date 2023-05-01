cb7c83efe53d5a09f411d08399cf5015dd697155
SleetFire <barkovskiy2000@gmail.com>
2023-05-01 22:55:59 +0300

    feat: add repo config

--- news_management_service/src/main/java/ru/clevertec/news_management_service/NewsManagementServiceApplication.java
+++ news_management_service/src/main/java/ru/clevertec/news_management_service/NewsManagementServiceApplication.java
@@ -2,8 +2,10 @@ package ru.clevertec.news_management_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "ru.clevertec.news_management_service.repository")
 public class NewsManagementServiceApplication {
 
 	public static void main(String[] args) {
