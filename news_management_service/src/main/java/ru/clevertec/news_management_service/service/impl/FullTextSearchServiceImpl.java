440242392adac500f8de807a073e9115a8891caa
SleetFire <barkovskiy2000@gmail.com>
2023-05-03 23:27:37 +0300

    feat: add full text search service

---
+++ news_management_service/src/main/java/ru/clevertec/news_management_service/service/impl/FullTextSearchServiceImpl.java
@@ -0,0 +1,80 @@
+package ru.clevertec.news_management_service.service.impl;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.clevertec.news_management_service.dto.CommentDto;
+import ru.clevertec.news_management_service.dto.NewsCommentsDtoWrapper;
+import ru.clevertec.news_management_service.dto.NewsDto;
+import ru.clevertec.news_management_service.dto.PageDto;
+import ru.clevertec.news_management_service.exception.EntityNotFoundException;
+import ru.clevertec.news_management_service.service.CommentService;
+import ru.clevertec.news_management_service.service.FullTextSearchService;
+import ru.clevertec.news_management_service.service.NewsService;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+@Transactional(readOnly = true)
+public class FullTextSearchServiceImpl implements FullTextSearchService {
+
+    private final NewsService newsServiceImpl;
+    private final CommentService commentServiceImpl;
+
+    public FullTextSearchServiceImpl(NewsService newsServiceImpl, CommentService commentServiceImpl) {
+        this.newsServiceImpl = newsServiceImpl;
+        this.commentServiceImpl = commentServiceImpl;
+    }
+
+    @Override
+    public List<NewsCommentsDtoWrapper> find(String text) {
+        List<NewsCommentsDtoWrapper> result = new ArrayList<>();
+        List<CommentDto> commentDtoList = commentServiceImpl.findAllByWordParts(text);
+        List<NewsDto> newsDtoList = newsServiceImpl.findAllByWordParts(text);
+
+        Set<Long> newsIdSetFromComments = commentDtoList.stream()
+                .map(commentDto -> commentDto.getNews().getId())
+                .collect(Collectors.toSet());
+        Set<Long> idSet = newsDtoList.stream()
+                .map(NewsDto::getId)
+                .collect(Collectors.toSet());
+        newsIdSetFromComments.addAll(idSet);
+
+        newsIdSetFromComments.forEach(id -> {
+            NewsDto newsDto = newsServiceImpl.findById(id);
+            List<CommentDto> comments = commentServiceImpl.findAllByNewsId(id);
+            result.add(NewsCommentsDtoWrapper.of(newsDto, comments));
+        });
+
+        if (result.isEmpty()) {
+            throw new EntityNotFoundException();
+        }
+
+        return result;
+    }
+
+    @Override
+    public List<NewsDto> findNewsList(String text) {
+        return newsServiceImpl.findAllByWordParts(text);
+    }
+
+    @Override
+    public List<CommentDto> findCommentList(String text) {
+        return commentServiceImpl.findAllByWordParts(text);
+    }
+
+    @Override
+    public PageDto<NewsDto> findNewsPage(String text, Pageable pageable) {
+        return newsServiceImpl.findPageByWordParts(text, pageable);
+    }
+
+    @Override
+    public PageDto<CommentDto> findCommentPage(String text, Pageable pageable) {
+        return commentServiceImpl.findPageByWordParts(text, pageable);
+    }
+}
