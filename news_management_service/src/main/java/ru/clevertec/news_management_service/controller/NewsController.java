beeb4f5dde8589ea502175f86dc9720e335991c3
SleetFire <barkovskiy2000@gmail.com>
2023-05-05 16:08:11 +0300

    feat: add endpoint for advice news service

--- news_management_service/src/main/java/ru/clevertec/news_management_service/controller/NewsController.java
+++ news_management_service/src/main/java/ru/clevertec/news_management_service/controller/NewsController.java
@@ -1,71 +1,77 @@
 package ru.clevertec.news_management_service.controller;
 
 import jakarta.validation.Valid;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.clevertec.news_management_service.dto.CreateNewsDto;
+import ru.clevertec.news_management_service.dto.NewsCommentsPageWrapperDto;
 import ru.clevertec.news_management_service.dto.NewsDto;
 import ru.clevertec.news_management_service.dto.PageDto;
-import ru.clevertec.news_management_service.exception.IllegalRequestParamException;
-import ru.clevertec.news_management_service.service.NewsService;
+import ru.clevertec.news_management_service.service.NewsServiceAdvance;
 
 import java.util.List;
+
+import static ru.clevertec.news_management_service.controller.util.ControllerUtil.*;
 
 @RestController
 @RequestMapping("/news")
 public class NewsController {
 
-    private final NewsService newsService;
+    private final NewsServiceAdvance newsServiceAdvanceImpl;
 
-    public NewsController(NewsService newsService) {
-        this.newsService = newsService;
+    public NewsController(NewsServiceAdvance newsServiceAdvanceImpl) {
+        this.newsServiceAdvanceImpl = newsServiceAdvanceImpl;
     }
 
     @PostMapping(value = "/",
             consumes = {MediaType.APPLICATION_JSON_VALUE},
             produces = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<NewsDto> create(@Valid @RequestBody CreateNewsDto news) {
-        return new ResponseEntity<>(newsService.create(news), HttpStatus.CREATED);
+        return new ResponseEntity<>(newsServiceAdvanceImpl.create(news), HttpStatus.CREATED);
     }
 
     @GetMapping(value = "/{id}", produces = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<NewsDto> findById(@PathVariable long id) {
-        return new ResponseEntity<>(newsService.findById(id), HttpStatus.OK);
+        return new ResponseEntity<>(newsServiceAdvanceImpl.findById(id), HttpStatus.OK);
     }
 
     @GetMapping(value = "/", produces = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<List<NewsDto>> findAll() {
-        return new ResponseEntity<>(newsService.findAll(), HttpStatus.OK);
+        return new ResponseEntity<>(newsServiceAdvanceImpl.findAll(), HttpStatus.OK);
     }
 
     @GetMapping(value = "/page", produces = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<PageDto<NewsDto>> findPage(@RequestParam(defaultValue = "0", required = false) int page,
                                                      @RequestParam(defaultValue = "1", required = false) int size) {
-        if (page < 0) {
-            throw new IllegalRequestParamException();
-        }
-        if (size < 1) {
-            throw new IllegalRequestParamException();
-        }
-        Pageable pageable = PageRequest.of(page, size);
 
-        return new ResponseEntity<>(newsService.findPage(pageable), HttpStatus.OK);
+        Pageable pageable = getPageable(page, size);
+
+        return new ResponseEntity<>(newsServiceAdvanceImpl.findPage(pageable), HttpStatus.OK);
     }
 
     @PatchMapping(value = "/{id}",
             consumes = {MediaType.APPLICATION_JSON_VALUE},
             produces = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<NewsDto> update(@PathVariable long id, @RequestBody CreateNewsDto updatedNews) {
-        return new ResponseEntity<>(newsService.update(id, updatedNews), HttpStatus.OK);
+        return new ResponseEntity<>(newsServiceAdvanceImpl.update(id, updatedNews), HttpStatus.OK);
     }
 
     @DeleteMapping(value = "/{id}")
     public void delete(@PathVariable long id) {
-        newsService.deleteById(id);
+        newsServiceAdvanceImpl.deleteById(id);
+    }
+
+    @GetMapping(value = "/{id}/comments/wrapper")
+    public ResponseEntity<NewsCommentsPageWrapperDto> findWrapper(@PathVariable long id,
+                                                                  @RequestParam(defaultValue = "0", required = false) int page,
+                                                                  @RequestParam(defaultValue = "1", required = false) int size) {
+
+        Pageable pageable = getPageable(page, size);
+
+        return new ResponseEntity<>(newsServiceAdvanceImpl.findNewsCommentsPageById(id, pageable), HttpStatus.OK);
     }
 
 }
