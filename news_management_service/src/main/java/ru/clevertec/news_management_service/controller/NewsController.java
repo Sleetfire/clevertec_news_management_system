49ab08c64b9f40887eb3e0f5f30b804a3b009bf2
SleetFire <barkovskiy2000@gmail.com>
2023-05-10 00:11:43 +0300

    fix: unwrap imports

--- news_management_service/src/main/java/ru/clevertec/news_management_service/controller/NewsController.java
+++ news_management_service/src/main/java/ru/clevertec/news_management_service/controller/NewsController.java
@@ -5,7 +5,15 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import ru.clevertec.news_management_service.dto.CreateNewsDto;
 import ru.clevertec.news_management_service.dto.NewsCommentsPageWrapperDto;
 import ru.clevertec.news_management_service.dto.NewsDto;
@@ -60,8 +68,9 @@ public class NewsController {
     }
 
     @DeleteMapping(value = "/{id}")
-    public void delete(@PathVariable long id) {
+    public ResponseEntity<Void> delete(@PathVariable long id) {
         newsServiceAdvanceImpl.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @GetMapping(value = "/{id}/comments/wrapper")
