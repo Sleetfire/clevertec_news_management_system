56b709627df3755c9fc4ef753cb115f0ec9f4182
SleetFire <barkovskiy2000@gmail.com>
2023-05-10 00:14:19 +0300

    feat: add cache comment aspect

---
+++ news_management_service/src/main/java/ru/clevertec/news_management_service/aop/CommentCacheAspect.java
@@ -0,0 +1,68 @@
+package ru.clevertec.news_management_service.aop;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.springframework.stereotype.Component;
+import ru.clevertec.news_management_service.dto.CommentDto;
+import ru.clevertec.news_management_service.service.Cache;
+import ru.clevertec.news_management_service.service.CacheFactory;
+
+import java.util.Optional;
+
+@Aspect
+@Component
+public class CommentCacheAspect {
+
+    private final Cache<Long, CommentDto> commentCache;
+
+    public CommentCacheAspect(CacheFactory cacheFactory) {
+        commentCache = cacheFactory.getCommentCache();
+    }
+
+    @Pointcut("execution(* ru.clevertec.news_management_service.service.impl.CommentServiceImpl.create(..))")
+    public void addCommentMethod() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Pointcut("execution(* ru.clevertec.news_management_service.service.impl.CommentServiceImpl.findById(*))")
+    public void findCommentMethod() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Pointcut("execution(* ru.clevertec.news_management_service.service.impl.CommentServiceImpl.update(..))")
+    public void updateCommentMethod() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Pointcut("execution(void ru.clevertec.news_management_service.service.impl.CommentServiceImpl.deleteById(*))")
+    public void deleteCommentMethod() {
+        throw new UnsupportedOperationException();
+    }
+
+    @AfterReturning(pointcut = "addCommentMethod() || updateCommentMethod()", returning = "retVal")
+    public Object addOrUpdateCommentToCache(Object retVal) {
+        CommentDto commentDto = (CommentDto) retVal;
+        long id = commentDto.getId();
+        commentCache.add(id, commentDto);
+        return commentDto;
+    }
+
+    @Around(value = "findCommentMethod()")
+    public Object findNewsInCache(ProceedingJoinPoint joinPoint) throws Throwable {
+        long id = (long) joinPoint.getArgs()[0];
+        Optional<CommentDto> optionalCommentDto = commentCache.get(id);
+        if (optionalCommentDto.isEmpty()) {
+            CommentDto commentDto = (CommentDto) joinPoint.proceed();
+            commentCache.add(id, commentDto);
+            return commentDto;
+        }
+        return optionalCommentDto.get();
+    }
+
+    @After(value = "deleteCommentMethod()")
+    public void deleteNewsInCache(JoinPoint joinPoint) {
+        long id = (long) joinPoint.getArgs()[0];
+        commentCache.delete(id);
+    }
+}
