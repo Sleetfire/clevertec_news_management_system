af664c9661a38eb919627d298bee177f2b2b2b72
SleetFire <barkovskiy2000@gmail.com>
2023-05-12 14:40:27 +0300

    test: add last methods

--- news_management_service/src/test/java/ru/clevertec/news_management_service/unit/service/impl/CommentServiceImplTest.java
+++ news_management_service/src/test/java/ru/clevertec/news_management_service/unit/service/impl/CommentServiceImplTest.java
@@ -7,8 +7,13 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import ru.clevertec.news_management_service.dto.CommentDto;
 import ru.clevertec.news_management_service.dto.CreateCommentDto;
+import ru.clevertec.news_management_service.dto.PageDto;
 import ru.clevertec.news_management_service.exception.EntityNotFoundException;
 import ru.clevertec.news_management_service.mapper.CommentMapper;
 import ru.clevertec.news_management_service.repository.CommentRepository;
@@ -21,10 +26,14 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.mockito.Mockito.*;
-import static ru.clevertec.news_management_service.testbuilder.CommentDtoFixture.*;
-import static ru.clevertec.news_management_service.testbuilder.NewsDtoFixture.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.verify;
+import static ru.clevertec.news_management_service.testbuilder.CommentDtoFixture.COMMENT_DTO;
+import static ru.clevertec.news_management_service.testbuilder.CommentDtoFixture.getCommentDtoList;
+import static ru.clevertec.news_management_service.testbuilder.NewsDtoFixture.NEWS_DTO;
 
 @SpringBootTest
 @ExtendWith(MockitoExtension.class)
@@ -44,7 +53,7 @@ class CommentServiceImplTest {
     private CommentServiceImpl commentServiceImpl;
 
     @Test
-    void checkCreateShouldCallTestedMethod() {
+    void checkCreateShouldReturnCommentDto() {
         CreateCommentDto createCommentDto = new CreateCommentDtoBuilder().build();
         CommentDto expectedCommentDto = COMMENT_DTO;
         Comment comment = commentMapper.toEntity(expectedCommentDto);
@@ -119,15 +128,103 @@ class CommentServiceImplTest {
     }
 
     @Test
-    void findPageByNewsId() {
+    void checkFindPageByNewsIdShouldReturnCommentPageDto() {
+        List<CommentDto> commentDtoList = getCommentDtoList(3);
+        List<Comment> content = commentMapper.toEntity(commentDtoList);
+        Pageable pageable = PageRequest.of(0, 1);
+        Page<Comment> page = new PageImpl<>(content, pageable, content.size());
+        long newsId = 1L;
+
+        doReturn(page)
+                .when(commentRepository)
+                .findAllByNewsId(newsId, pageable);
+
+        PageDto<CommentDto> actualPageDto = commentServiceImpl.findPageByNewsId(newsId, pageable);
+
+        assertThat(actualPageDto)
+                .extracting(PageDto::getNumber, PageDto::getSize, PageDto::getTotalPages, PageDto::getNumberOfElements,
+                        PageDto::isFirst, PageDto::getNumberOfElements, PageDto::isLast, PageDto::getContent)
+                .containsExactly(0, 1, 3, 3, true, 3, false, commentDtoList);
     }
 
     @Test
-    void update() {
+    void checkFindPageByNewsIdShouldThrowEntityNotFoundException() {
+        Page<Comment> page = Page.empty();
+        Pageable pageable = PageRequest.of(0, 1);
+        long newsId = 1L;
+
+        doReturn(page)
+                .when(commentRepository)
+                .findAllByNewsId(newsId, pageable);
+
+        assertThatThrownBy(() -> commentServiceImpl.findPageByNewsId(newsId, pageable))
+                .isInstanceOf(EntityNotFoundException.class);
     }
 
     @Test
-    void deleteById() {
+    void checkUpdateShouldCallTestedMethod() {
+        CreateCommentDto updated = new CreateCommentDtoBuilder()
+                .withText("updated-text")
+                .withUsername("updated-username")
+                .build();
+        Comment comment = commentMapper.toEntity(COMMENT_DTO);
+        Optional<Comment> optionalComment = Optional.of(comment);
+        long id = 1L;
+
+        doReturn(optionalComment)
+                .when(commentRepository)
+                .findById(id);
+
+        commentServiceImpl.update(id, updated);
+
+        verify(commentRepository)
+                .save(any(Comment.class));
+    }
+
+    @Test
+    void checkUpdateShouldThrowEntityNotFoundException() {
+        Optional<Comment> commentOptional = Optional.empty();
+        CreateCommentDto updated = new CreateCommentDtoBuilder()
+                .withText("updated-text")
+                .withUsername("updated-username")
+                .build();
+        long id = 1L;
+
+        doReturn(commentOptional)
+                .when(commentRepository)
+                .findById(id);
+
+        assertThatThrownBy(() -> commentServiceImpl.update(id, updated))
+                .isInstanceOf(EntityNotFoundException.class);
+    }
+
+    @Test
+    void checkDeleteByIdShouldCallTestedMethod() {
+        Comment comment = commentMapper.toEntity(COMMENT_DTO);
+        Optional<Comment> optionalComment = Optional.of(comment);
+        long id = 1L;
+
+        doReturn(optionalComment)
+                .when(commentRepository)
+                .findById(id);
+
+        commentServiceImpl.deleteById(id);
+
+        verify(commentRepository)
+                .deleteById(id);
+    }
+
+    @Test
+    void checkDeleteByIdShouldThrowEntityNotFoundException() {
+        Optional<Comment> commentOptional = Optional.empty();
+        long id = 1L;
+
+        doReturn(commentOptional)
+                .when(commentRepository)
+                .findById(id);
+
+        assertThatThrownBy(() -> commentServiceImpl.deleteById(id))
+                .isInstanceOf(EntityNotFoundException.class);
     }
 
     @Test
@@ -159,14 +256,66 @@ class CommentServiceImplTest {
     }
 
     @Test
-    void deleteAllByNewsId() {
+    void checkDeleteAllByNewsIdShouldCallTestedMethod() {
+        long newsId = 1L;
+
+        commentServiceImpl.deleteAllByNewsId(newsId);
+
+        verify(commentRepository)
+                .deleteAllByNewsId(newsId);
     }
 
     @Test
-    void findAllByWordParts() {
+    void checkFindAllByWordPartsShouldReturnListOfCommentDto() {
+        List<CommentDto> expectedCommentDtoList = getCommentDtoList(3);
+        List<Comment> commentList = commentMapper.toEntity(expectedCommentDtoList);
+        String wordPart = "default";
+        String searchWordPart = "%" + wordPart + "%";
+
+        doReturn(commentList)
+                .when(commentRepository)
+                .findAllByWordParts(searchWordPart);
+
+        List<CommentDto> actualCommentDtoList = commentServiceImpl.findAllByWordParts(wordPart);
+
+        assertThat(actualCommentDtoList)
+                .hasSameElementsAs(expectedCommentDtoList);
     }
 
     @Test
-    void findPageByWordParts() {
+    void checkFindAllByWordPartsShouldThrowEntityNotFoundException() {
+        String wordPart = "default";
+        String searchWordPart = "%" + wordPart + "%";
+
+        doReturn(Collections.emptyList())
+                .when(commentRepository)
+                .findAllByWordParts(searchWordPart);
+
+        List<CommentDto> actualCommentDtoList = commentServiceImpl.findAllByWordParts(wordPart);
+
+        assertThat(actualCommentDtoList)
+                .isEmpty();
     }
+
+    @Test
+    void checkFindPageByWordPartsShouldReturnPageDto() {
+        List<CommentDto> commentDtoList = getCommentDtoList(3);
+        List<Comment> content = commentMapper.toEntity(commentDtoList);
+        Pageable pageable = PageRequest.of(0, 1);
+        Page<Comment> page = new PageImpl<>(content, pageable, content.size());
+        String wordPart = "default";
+        String searchWordPart = "%" + wordPart + "%";
+
+        doReturn(page)
+                .when(commentRepository)
+                .findAllByWordParts(searchWordPart, pageable);
+
+        PageDto<CommentDto> actualPageDto = commentServiceImpl.findPageByWordParts(wordPart, pageable);
+
+        assertThat(actualPageDto)
+                .extracting(PageDto::getNumber, PageDto::getSize, PageDto::getTotalPages, PageDto::getNumberOfElements,
+                        PageDto::isFirst, PageDto::getNumberOfElements, PageDto::isLast, PageDto::getContent)
+                .containsExactly(0, 1, 3, 3, true, 3, false, commentDtoList);
+    }
+
 }
