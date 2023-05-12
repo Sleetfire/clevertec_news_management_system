fdab4c20d5106d062efefe90a24c953152f250db
SleetFire <barkovskiy2000@gmail.com>
2023-05-12 23:06:43 +0300

    test: add base integration test

---
+++ news_management_service/src/test/java/ru/clevertec/news_management_service/integration/BaseIntegrationTest.java
@@ -0,0 +1,30 @@
+package ru.clevertec.news_management_service.integration;
+
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.jdbc.Sql;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+@Sql("classpath:db/data.sql")
+@ActiveProfiles("test")
+@Transactional
+@SpringBootTest
+public abstract class BaseIntegrationTest {
+
+    private static final PostgreSQLContainer<?> container = new PostgreSQLContainer<>();
+
+    @BeforeAll
+    static void init() {
+        container.start();
+    }
+
+    @DynamicPropertySource
+    static void setup(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", container::getJdbcUrl);
+    }
+
+}
