df7159e26392d222768a780c1ba61336d52487b1
SleetFire <barkovskiy2000@gmail.com>
2023-05-12 16:03:25 +0300

    feat: add exceptions handling

--- news_management_service/src/main/java/ru/clevertec/news_management_service/controller/advice/GlobalExceptionAdvice.java
+++ news_management_service/src/main/java/ru/clevertec/news_management_service/controller/advice/GlobalExceptionAdvice.java
@@ -8,11 +8,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import ru.clevertec.news_management_service.dto.error.MultipleResponseError;
+import ru.clevertec.news_management_service.dto.error.SingleResponseError;
+import ru.clevertec.news_management_service.dto.error.ValidationError;
+import ru.clevertec.news_management_service.exception.EntityNotFoundException;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 @ControllerAdvice
 public class GlobalExceptionAdvice extends ResponseEntityExceptionHandler {
@@ -20,13 +25,21 @@ public class GlobalExceptionAdvice extends ResponseEntityExceptionHandler {
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers,
                                                                   HttpStatusCode status, WebRequest request) {
-        Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getAllErrors().forEach(error ->{
+        List<ValidationError> errors = new ArrayList<>();
+        ex.getBindingResult().getAllErrors().forEach(error -> {
 
-            String fieldName = ((FieldError) error).getField();
+            String field = ((FieldError) error).getField();
             String message = error.getDefaultMessage();
-            errors.put(fieldName, message);
+            ValidationError validationError = new ValidationError(field, message);
+            errors.add(validationError);
         });
-        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+
+        MultipleResponseError<ValidationError> error = new MultipleResponseError<>("Validation error", errors);
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<SingleResponseError> handleEntityNotFoundException(EntityNotFoundException exception) {
+        return new ResponseEntity<>(new SingleResponseError(exception.getMessage()), HttpStatus.NOT_FOUND);
     }
 }
