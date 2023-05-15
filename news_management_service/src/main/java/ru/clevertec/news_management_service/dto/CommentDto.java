4b5f9a16d34720bc5152ead29871b317db1af429
SleetFire <barkovskiy2000@gmail.com>
2023-05-16 00:06:52 +0300

    feat: newsId serializing

--- news_management_service/src/main/java/ru/clevertec/news_management_service/dto/CommentDto.java
+++ news_management_service/src/main/java/ru/clevertec/news_management_service/dto/CommentDto.java
@@ -1,9 +1,12 @@
 package ru.clevertec.news_management_service.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import ru.clevertec.news_management_service.dto.serializer.NewsDtoSerializer;
 
 import java.time.LocalDateTime;
 
@@ -13,10 +16,16 @@ import java.time.LocalDateTime;
 @Builder
 public class CommentDto {
 
+    @JsonProperty("id")
     private long id;
+    @JsonProperty("time")
     private LocalDateTime time;
+    @JsonProperty("text")
     private String text;
+    @JsonProperty("username")
     private String username;
+    @JsonProperty("newsId")
+    @JsonSerialize(using = NewsDtoSerializer.class)
     private NewsDto news;
 
 }
