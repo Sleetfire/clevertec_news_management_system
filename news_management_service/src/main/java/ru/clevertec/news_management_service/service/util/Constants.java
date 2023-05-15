7f7d112021cd66459d5141982458c1411e271ded
SleetFire <barkovskiy2000@gmail.com>
2023-05-15 16:46:36 +0300

    feat: add service's error messages

---
+++ news_management_service/src/main/java/ru/clevertec/news_management_service/service/util/Constants.java
@@ -0,0 +1,22 @@
+package ru.clevertec.news_management_service.service.util;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class Constants {
+
+    public static final String NEWS_NOT_FOUND_MESSAGE = "News wasn't found";
+
+    public static final String NEWS_LIST_NOT_FOUND_MESSAGE = "List of news wasn't found";
+
+    public static final String NEWS_PAGE_NOT_FOUND_MESSAGE = "Page of news wasn't found";
+
+    public static final String COMMENT_NOT_FOUND_MESSAGE = "Comment wasn't found";
+
+    public static final String COMMENT_LIST_NOT_FOUND_MESSAGE = "List of comments wasn't found";
+
+    public static final String COMMENT_PAGE_NOT_FOUND_MESSAGE = "Page of comments wasn't found";
+
+    public static final String ENTITY_NOT_FOUND_MESSAGE = "Entity wasn't found";
+
+}
