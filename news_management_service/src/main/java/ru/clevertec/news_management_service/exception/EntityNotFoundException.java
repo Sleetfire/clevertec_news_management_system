3be5804926651b9ae056b430de067210fd987a0f
SleetFire <barkovskiy2000@gmail.com>
2023-05-15 16:46:59 +0300

    feat: add constructor for error message

--- news_management_service/src/main/java/ru/clevertec/news_management_service/exception/EntityNotFoundException.java
+++ news_management_service/src/main/java/ru/clevertec/news_management_service/exception/EntityNotFoundException.java
@@ -2,4 +2,7 @@ package ru.clevertec.news_management_service.exception;
 
 public class EntityNotFoundException extends RuntimeException {
 
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
 }
